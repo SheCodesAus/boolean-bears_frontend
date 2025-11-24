c65a7d140f0e968927996bd59a246cb79e9999ef
stephanite9 <stephanite.chan@gmail.com>
2025-11-24 20:52:09 +0800

    added auth components and hook, updated nav bar with Hello User, fixed api token call

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import CoursePage from "./pages/CoursePage.jsx";
 import LoginPage from "./pages/LoginPage.jsx";
 
 import NavBar from "./components/NavBar.jsx";
+import { AuthProvider } from './components/AuthProvider.jsx';
 
 const router = createBrowserRouter([
   {
@@ -17,12 +18,14 @@ const router = createBrowserRouter([
           { path: "/login", element: <LoginPage /> },
           { path: "/course/:id", element: <CoursePage /> },
       ],
-  },
-]);
+    },
+  ],
+);
 
-  ReactDOM.createRoot(document.getElementById("root")).render(
-    <React.StrictMode>
-      {/* Here we wrap our app in the router provider so they render */}
-      <RouterProvider router={router} />
-    </React.StrictMode>
-  );
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
+  </React.StrictMode>
+);
