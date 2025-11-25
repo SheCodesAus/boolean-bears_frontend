2fd890c79e219a432c3dfb1d7d92a6bb5ef48bd2
LYCIA <HACERILHAN@HOTMAIL.FR>
2025-11-25 22:05:58 +0800

    each course will display a related image. those can be changed later when we find nice images.

--- src/components/CourseCard.jsx
+++ src/components/CourseCard.jsx
@@ -1,5 +1,17 @@
 import { Link } from "react-router-dom";
 import "./CourseCard.css";
+const categoryImages = {
+    "science and technology": "src/assets/arts_crafts.jpeg",
+    "arts and crafts": "src//assets/arts_crafts.jpeg",
+    "reading and writing": "src//assets/reading_writing.jpeg",
+    "music and musical instruments": "src/assets/music.jpeg",
+    "language learning": "src//assets/language.jpeg",
+    "health and wellness": "src//assets/health.jpeg",
+    "business and finance": "src//assets/business.jpeg",
+    "personal development": "src//assets/personal_dev.jpeg",
+    "other": "src//assets/other.jpeg",
+};
+
 
 function CourseCard(props) {
     const { courseData } = props;
@@ -12,7 +24,12 @@ function CourseCard(props) {
                 <p>Category: {courseData.category}</p>
                 <p>By: {courseData.owner}</p>
                 <p>{courseData.course_content.substring(0, 100)}...</p>
-            
+                <img 
+                    src={categoryImages[courseData.category]} 
+                    alt={courseData.category} 
+                    className="course-image"
+                />
+
             {/* Add the Button Here */}
             <button className="btn-learn-more">
                 Learn More
