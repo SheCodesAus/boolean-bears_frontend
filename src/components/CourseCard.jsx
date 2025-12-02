3a8c4c0b2312ca69ca6e412c2c0e7ccdfe429347
QIN SHEN <sharondaisy62@gmail.com>
2025-12-02 19:13:29 +0800

    update the accessibility of course card

--- src/components/CourseCard.jsx
+++ src/components/CourseCard.jsx
@@ -1,31 +1,48 @@
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../hooks/use-auth"
 import "./CourseCard.css";
 import categoryImages from "../utils/category-images";
 import { categoryDisplay } from "../utils/category-display";
 
+
 function CourseCard(props) {
     const { courseData } = props;
-    const courseLink = `course/${courseData.id}`;
+
+    // Initialize hooks
+    const { auth } = useAuth();
+    const navigate = useNavigate();
+    
     const likesCount = courseData?.likes_count ?? courseData?.likes ?? 0;
+
+    // Create the click handler
+    const handleCardClick = () => {
+        // Check if token exists in auth object
+        if (auth && auth.token) {
+            // User is logged in -> go to course page
+            navigate(`/course/${courseData.id}`);
+        // User is NOT logged in -> Go to Login Page
+        } else {
+            alert("Please log in to view course details.");
+            navigate("/login")
+        }
+    };
 
     return (
         <div className="course-card">
-            <Link to={courseLink}>
-                <h2>{courseData.title}</h2>
-                <p>Category: {categoryDisplay[courseData.category] || courseData.category}</p>
-                <p>By: {courseData.owner}</p>
-                <p>{courseData.brief_description.substring(0, 150)}...</p>
-                <img 
-                    src={categoryImages[courseData.category]} 
-                    alt={courseData.category} 
-                    className="course-image"
+            <h2>{courseData.title}</h2>
+            <p>Category: {categoryDisplay[courseData.category] || courseData.category}</p>
+            <p>By: {courseData.owner}</p>
+            <p>{courseData.brief_description.substring(0, 150)}...</p>
+            <img 
+                src={categoryImages[courseData.category]} 
+                alt={courseData.category} 
+                className="course-image"
                 />
-                <span className="likes">❤️ {likesCount}</span>
+            <span className="likes">❤️ {likesCount}</span>
 
-            <button className="btn-learn-more">
+            <button className="btn-learn-more" onClick={handleCardClick}>
                 Learn More
             </button>
-            </Link>
         </div>
     );
 }
